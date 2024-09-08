03f9e7ac89fcfb3a430496c6fe5fcff45d389331
Andrew Mottes <ajmottes@gmail.com>
2024-09-08 18:55:35 -0400

    Finish Introduction and Django Admin

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from blog.models import Tag, Post
+
+# Register your models here.
+admin.site.register(Tag)
+
+class PostAdmin(admin.ModelAdmin):
+    # auto-create the slug field from the title
+    prepopulated_fields = {"slug": ("title",)}
+    # the following line controls the columns displayed in list view of admin site
+    list_display = ('title', 'author', 'created_at', 'modified_at', 'published_at')
+
+admin.site.register(Post, PostAdmin)
