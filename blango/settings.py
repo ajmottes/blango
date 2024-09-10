26d3c30cf9fe548bdbbd440e0f8131181f861c66
ajmottes <ajmottes@gmail.com>
2024-09-10 16:26:34 -0400

    "Finish security ad passwords"

--- blango/settings.py
+++ blango/settings.py
@@ -93,6 +93,13 @@ class Dev(Configuration):
     #  Default method is commented out above
     DATABASES = values.DatabaseURLValue(f"sqlite:///{BASE_DIR}/db.sqlite3")
 
+    PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.Argon2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
+
     # Password validation
     # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
