783db3f995046e585082dd5aa681c6e2ed252857
ajmottes <ajmottes@gmail.com>
2024-09-14 14:58:59 -0400

    "Finish nested relationships"

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
-from blog.models import Post, Tag
+
+from blog.models import Post, Tag, Comment
 from blango_auth.models import User
 
 
+class TagField(serializers.SlugRelatedField):
+    def to_internal_value(self, data):
+        try:
+            return self.get_queryset().get_or_create(value=data.lower())[0]
+        except (TypeError, ValueError):
+            self.fail(f"Tag value {data} is invalid")
+
+
 class PostSerializer(serializers.ModelSerializer):
-    tags = serializers.SlugRelatedField(
+    tags = TagField(
         slug_field="value", many=True, queryset=Tag.objects.all()
     )
 
@@ -22,3 +31,40 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email"]
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    # set required to false so we can recognize newly created comments
+    #   only newly created comments will not have an id
+    id = serializers.IntegerField(required=False)
+    creator = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ["id", "creator", "content", "modified_at", "created_at"]
+        readonly = ["modified_at", "created_at"]
+
+
+# we only want comments displayed in the detail view (NOT list view)
+class PostDetailSerializer(PostSerializer):
+    comments = CommentSerializer(many=True)
+
+    def update(self, instance, validated_data):
+        # remove comments from data before calling super
+        #   we want to handle the comments ourselves
+        comments = validated_data.pop("comments")
+
+        instance = super(PostDetailSerializer, self).update(instance, validated_data)
+
+        # save any newly created comments (which will NOT have an id)
+        for comment_data in comments:
+            if comment_data.get("id"):
+                # comment has an ID so was pre-existing
+                continue
+            comment = Comment(**comment_data)
+            comment.creator = self.context["request"].user
+            comment.content_object = instance
+            comment.save()
+
+        return instance
+
