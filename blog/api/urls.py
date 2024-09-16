867ac93120aa9d8d410a4f83bf5637d4d1e18cb3
ajmottes <ajmottes@gmail.com>
2024-09-16 06:16:50 -0400

    "Finish filtering"

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -33,6 +33,11 @@ urlpatterns += [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("", include(router.urls)),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
 ]
 
 # urlpatterns = format_suffix_patterns(urlpatterns)
